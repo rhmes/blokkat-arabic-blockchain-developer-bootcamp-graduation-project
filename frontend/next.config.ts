9fc478df4d6d8ae8301a0ace2e9cfa2fae233730
r <eng.raghdaa@gmail.com>
2025-06-02 18:08:23 +0300

    Update config for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      stream: false,
+    };
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
